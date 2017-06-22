ed3dcde2898b0e8b96abe42a5f2b09202a7abf7e
root <root@VM_103_136_centos.(none)>
2017-06-22 14:48:36 +0800

    del: sh

--- nginx/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker run --name my-custom-nginx-container \
-  -v `pwd`/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
-  -v `pwd`/html:/etc/nginx/html \
-  -v `pwd`/sbin:/etc/nginx/sbin \
-  -v `pwd`/static:/etc/nginx/static \
-  -v `pwd`/logs:/etc/nginx/logs \
-  -p 80:80 \
-  -d nginx
